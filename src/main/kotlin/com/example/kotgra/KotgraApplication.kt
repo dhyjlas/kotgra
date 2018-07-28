c8f33bcb2b9a47cb980d730ed49879d554d77377
dhyjlas <dhyjlas@163.com>
2018-07-28 10:40:32 +0800

    java+kotlin 混用demo项目

---
+++ src/main/kotlin/com/example/kotgra/KotgraApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotgra
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotgraApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotgraApplication>(*args)
+}
